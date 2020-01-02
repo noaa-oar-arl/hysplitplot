a158eafd88cfa2d8b2ee888b2a4912a7e6d7ad46
Sonny Zinn <sonny.zinn@noaa.gov>
2020-01-02 09:19:50 -0500

    Update the version number to 0.1.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name="hysplitplot",
-    version="0.1.5",
+    version="0.1.6",
     description="HYSPLIT Graphics",
     author="Sonny Zinn",
     author_email="sonny.zinn@noaa.gov",
